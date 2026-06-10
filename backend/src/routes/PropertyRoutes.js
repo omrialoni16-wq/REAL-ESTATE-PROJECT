62e065992e58ee4523c67d94540b3ee3093a8b43
Dandan-ftp <xdanbaron@gmail.com>
2026-06-10 13:33:41 +0300

    edit-button-and-func

--- backend/src/routes/PropertyRoutes.js
+++ backend/src/routes/PropertyRoutes.js
@@ -3,11 +3,13 @@ import {
   addProperty,
   getAllProperties,
   removeProperty,
+  editProperty,
 } from "../controllers/propertyController.js";
 
 const router = express.Router();
 
 router.post("/api/properties", addProperty);
 router.get("/api/properties", getAllProperties);
+router.put("/api/properties/:id", editProperty);
 
 export default router;
